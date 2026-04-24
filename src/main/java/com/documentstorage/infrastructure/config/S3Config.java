8cb854a2e3b27d785c86f6d1aa5b85a7c1c37a8a
Nathalie <nathalieribmartines@gmail.com>
2026-04-24 10:33:51 -0300

    chore: add AWS infrastructure configuration files
    - Add .env.example with AWS credentials template
    - Add task-definition.json for ECS Fargate deployment
    - Configure S3Config.java for document storage

--- src/main/java/com/documentstorage/infrastructure/config/S3Config.java
+++ src/main/java/com/documentstorage/infrastructure/config/S3Config.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.presigner.S3Presigner;
-import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
+import software.amazon.awssdk.auth.credentials.EnvironmentVariableCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 
 @Configuration
@@ -18,7 +18,7 @@ public class S3Config {
     public S3Client s3Client() {
         return S3Client.builder()
                 .region(Region.of(region))
-                .credentialsProvider(DefaultCredentialsProvider.create())
+                .credentialsProvider(EnvironmentVariableCredentialsProvider.create())
                 .build();
     }
 
@@ -26,7 +26,7 @@ public class S3Config {
     public S3Presigner s3Presigner() {
         return S3Presigner.builder()
                 .region(Region.of(region))
-                .credentialsProvider(DefaultCredentialsProvider.create())
+                .credentialsProvider(EnvironmentVariableCredentialsProvider.create())
                 .build();
     }
 }
